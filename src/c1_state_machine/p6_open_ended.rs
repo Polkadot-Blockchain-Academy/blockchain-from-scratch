31cd44abad579f6d33c6411e6e3e11ab4dbc73ce
Alina-rust-dev <rust.buzz@gmail.com>
2024-02-05 21:59:23 +0000

    first draft of tic tac toe logic

--- src/c1_state_machine/p6_open_ended.rs
+++ src/c1_state_machine/p6_open_ended.rs
@@ -23,16 +23,164 @@
 //!   - Reputation System
 
 use super::StateMachine;
+use std::io;
+enum ValueInCell{
+	X,
+	O
+}
+pub struct MoveMaker {
+	board: [[ValueInCell; 3]; 3],
+	player1: ValueInCell,
+	player2: ValueInCell,
+	is_next_player1: bool,
+	winner: Option<ValueInCell>
+}
 
-pub struct State {}
+pub enum Action {
+	make_move(i32,i32),
+	check_win,
+}
 
-pub enum Transition {}
+impl StateMachine for MoveMaker {
+	type State = MoveMaker;
+	type Transition = Action;
 
-impl StateMachine for State {
-	type State = State;
-	type Transition = Transition;
+	fn next_state(starting: &Self::State, t: &Self::Transition) -> Self::State {
+		match t{
+			&Action::print_board(board: &[[char; 3]; 3]) => {
+				for i in 0..3{
+					for j in 0..3{
+						print!("{}", board[i][j]);
+					}
+					println!();
+				}
 
-	fn next_state(_starting: &Self::State, _t: &Self::Transition) -> Self::State {
-		todo!()
+			&Action::make_move(board: &mut[[char; 3]; 3], player: &mut char) => {
+				let mut x = String::new();
+				let mut y = String::new();
+				println!("Please enter the row for your move:");
+				io::stdin().read_line(&mut x).unwrap();
+				println!("Please enter the column for your move:");
+				io::stdin().read_line(&mut y).unwrap();
+				
+				let x: usize = x.trim().parse().unwrap();
+				let y: usize = y.trim().parse().unwrap();
+			
+				if x >= 3 || y >= 3{
+					println!("Invalid move!");
+					make_move(board, player);
+				}
+				if board[x][y] != 'X' && board[x][y] != 'O'{
+					board[x][y] = *player;
+			
+					if *player == 'X'{
+						*player = 'O';
+					}
+					else{
+						*player = 'X';
+					}
+				}
+				else{
+					println!("Invalid move!");
+					make_move(board, player);
+				}
+			}
+
+			&Action::check_win(board: &[[char; 3]; 3], player: char) => {
+				for i in 0..3{
+					if board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+						return true;
+					}
+				}
+				for i in 0..3 {
+					if board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+						return true;
+					}
+				}
+				if board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+					return true;
+				}
+				if board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+					return true;
+				}
+				return false;
+			}
+
+		}
 	}
+}
+
+#[test]
+fn playing(){
+	let mut board = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']];
+	let mut player = 'X';
+
+	loop{
+		print_board(&board);
+		make_move(&mut board, &mut player);
+
+		if check_win(&board, player){
+			println!("Player {} wins!", player);
+			break;
+		}
+	}
+}
+
+fn print_board(board: &[[char; 3]; 3]){
+	for i in 0..3{
+		for j in 0..3{
+			print!("{}", board[i][j]);
+		}
+		println!();
+	}
+}
+fn make_move(board: &mut[[char; 3]; 3], player: &mut char){
+	let mut x = String::new();
+	let mut y = String::new();
+	println!("Please enter the row for your move:");
+	io::stdin().read_line(&mut x).unwrap();
+	println!("Please enter the column for your move:");
+	io::stdin().read_line(&mut y).unwrap();
+	
+	let x: usize = x.trim().parse().unwrap();
+	let y: usize = y.trim().parse().unwrap();
+
+	if x >= 3 || y >= 3{
+		println!("Invalid move!");
+		make_move(board, player);
+	}
+	if board[x][y] != 'X' && board[x][y] != 'O'{
+		board[x][y] = *player;
+
+		if *player == 'X'{
+			*player = 'O';
+		}
+		else{
+			*player = 'X';
+		}
+	}
+	else{
+		println!("Invalid move!");
+		make_move(board, player);
+	}
+}
+
+fn check_win(board: &[[char; 3]; 3], player: char) -> bool {
+	for i in 0..3{
+		if board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+			return true;
+		}
+	}
+	for i in 0..3 {
+		if board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+			return true;
+		}
+	}
+	if board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+		return true;
+	}
+	if board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+		return true;
+	}
+	return false;
 }
